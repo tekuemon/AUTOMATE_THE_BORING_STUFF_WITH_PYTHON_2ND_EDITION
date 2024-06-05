2e49d9ac3433f7eed308a9878f3cfc7561b14257
tekuemon <tekuemon@gmail.com>
2024-06-05 23:57:12 +0900

    part_1/chapter_2 first

---
+++ part_1/chapter_2/littlekid.py
@@ -0,0 +1,8 @@
+name = 'Carol'
+age = 3000
+if name == 'Alice' :
+    print('やぁ、Alice。')
+elif age < 12 :
+    print('Aliceじゃないね、お嬢ちゃん。')
+else :
+    print('君はAliceでも子供でもないね。')
---
+++ part_1/chapter_2/vampire2.py
@@ -0,0 +1,10 @@
+name = 'Carol'
+age = 3000
+if name == 'Alice' :
+    print('やぁ、Alice。')
+elif age < 12 :
+    print('Aliceじゃないね、お嬢ちゃん。')
+elif age > 100 : 
+    print('Aliceじゃないね、お婆ちゃん。')
+elif age  > 2000 :
+    print('Aliceはお前のような不死身ではない、吸血鬼め。')
